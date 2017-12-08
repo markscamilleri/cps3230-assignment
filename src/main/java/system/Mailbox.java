10358080d04beff7fbb3ef52f768e139020e4362
Miguel206 <dinglimiguel97@gmail.com>
2017-12-08 22:27:57 +0100

    Step back.

--- src/main/java/system/Mailbox.java
+++ src/main/java/system/Mailbox.java
@@ -48,6 +48,4 @@ public class Mailbox {
     public boolean addMessage(Message message) {
         return false;
     }
-
-    //todo: (Suggestion) addMessage returns false if targetAgentId not equal to ownerId ?
 }
