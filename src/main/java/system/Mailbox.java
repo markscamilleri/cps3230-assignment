3ae539d814b157f66aef78bdb881982c5dfeb041
Mark Said Camilleri <markscamilleri@gmail.com>
2017-12-16 21:39:29 +0100

    Pulled, and adde check to remove expired/deleted messages from mailbox queue

--- src/main/java/system/Mailbox.java
+++ src/main/java/system/Mailbox.java
@@ -10,7 +10,7 @@ import java.util.concurrent.LinkedBlockingQueue;
  * This class encapsulates the functionality of a mailbox that holds all messages for a user.
  */
 public class Mailbox {
-
+    
     public final static int MAX_MESSAGES = 25;
     public final static Duration TIME_LIMIT = Duration.ofMinutes(30);
 
@@ -18,41 +18,36 @@ public class Mailbox {
      * The id of the owner of the mailbox.
      */
     public final String ownerId;
-
+    
     /**
      * The list of unconsumed messages in the mailbox.
      */
     protected final Queue<Message> messages = new LinkedBlockingQueue<>(MAX_MESSAGES);
-
+    
     Mailbox(String ownerId) {
         this.ownerId = ownerId;
     }
-
+    
     /**
      * Returns the next message in the box on a FIFO basis.
      *
      * @return A message or null if the mailbox is empty.
      */
     public synchronized Message consumeNextMessage() {
-        Message message;
-
-        while (!Objects.isNull(message = messages.poll())) {
-            if (!message.isDeleted())
-                return message;
-        }
-
-        return null;
+        removeExpiredMessages();
+        return messages.poll();
     }
-
+    
     /**
      * Checks if there are any messages in the mailbox.
      *
      * @return true if there is at least one message in the mailbox.
      */
     public synchronized boolean hasMessages() {
+        removeExpiredMessages();
         return !messages.isEmpty();
     }
-
+    
     /**
      * Adds a message to the mailbox.
      *
@@ -60,11 +55,36 @@ public class Mailbox {
      * @return true if successful, false otherwise.
      */
     public synchronized boolean addMessage(Message message) {
+        removeExpiredMessages();
         return isValidMessage(message) && messages.offer(message);
     }
 
+    /**
+     * Checks if the message is valid
+     * @param message the message to check
+     * @return true if it is valid, false otherwise.
+     */
     private boolean isValidMessage(Message message) {
         return message.getTargetAgentId().equals(this.ownerId) &&
                 Instant.now().isBefore(message.getTimestamp().plus(TIME_LIMIT));
+    }
+    
+    /**
+     * Removes all expired messages from the mailbox.
+     * @return the amount of messages that got deleted
+     */
+    private int removeExpiredMessages() {
+        Message message;
+        int count = 0;
+        while (!Objects.isNull(message = messages.peek())) {
+            if (!message.isDeleted()) {
+                messages.poll();
+                count++;
+            } else {
+                break;
+            }
+        }
+        
+        return count;
     }
 }
