2ef72312eb2df6074bbefa42fa68db140a73375e
Miguel206 <dinglimiguel97@gmail.com>
2017-12-20 21:43:34 +0100

    Some messaging system tests.

--- src/test/java/system/MessagingSystemTest.java
+++ src/test/java/system/MessagingSystemTest.java
@@ -1,30 +1,50 @@
 package system;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
+import util.Utils;
+
+import java.lang.reflect.Field;
 
 public class MessagingSystemTest {
 
+    private MessagingSystem msgSystem;
+
     @Before
-    public void setUp() {
+    public void setUp() throws NoSuchFieldException, IllegalAccessException {
+        Field instance = MessagingSystem.class.getDeclaredField("INSTANCE");
+        instance.setAccessible(true);
+        instance.set(null, null);
+        msgSystem = MessagingSystem.getInstance();
     }
 
     @After
     public void tearDown() {
+        msgSystem = null;
     }
 
     @Test
-    public void registerLoginKey() {
+    public void registerFailsIfLoginKeyIncorrectLength() {
+        final String loginKey = Utils.getNRandomCharacters(MessagingSystem.LOGIN_KEY_LENGTH - 1);
+        final String agentId = "1234xy";
+
+        Assert.assertFalse(msgSystem.registerLoginKey(agentId, loginKey));
     }
 
     @Test
-    public void login() {
+    public void registerFailsIfLoginKeyNotUnique() {
+        final String loginKey = Utils.getNRandomCharacters(MessagingSystem.LOGIN_KEY_LENGTH);
+        final String agentId1 = "1234xy";
+        final String agentId2 = "5678ab";
+
+        Assume.assumeTrue(msgSystem.registerLoginKey(agentId1, loginKey));
+        Assert.assertFalse(msgSystem.registerLoginKey(agentId2, loginKey));
     }
 
     @Test
-    public void sendMessage() {
-        // todo: does not exceed 140 characters
-        // todo: does not contain blocked words
+    public void registerSucceedsIfLoginKeyValid() {
+        final String loginKey = Utils.getNRandomCharacters(MessagingSystem.LOGIN_KEY_LENGTH);
+        final String agentId = "1234xy";
+
+        Assert.assertTrue(msgSystem.registerLoginKey(agentId, loginKey));
     }
 }
