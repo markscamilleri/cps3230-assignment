5454562818229d94b823caa59241d6bbfc3f904f
Mark Said Camilleri <markscamilleri@gmail.com>
2017-12-20 17:41:47 +0100

    Added javadoc comment for Timeoutable

--- src/main/java/util/Timeoutable.java
+++ src/main/java/util/Timeoutable.java
@@ -2,6 +2,15 @@ package util;
 
 import java.time.Instant;
 
+/**
+ * An object that would timeout must extend this class.
+ * It provides functionality for marking this object as deleted
+ *
+ * For the timeout functionality, this must be used with a timeout
+ * handler
+ *
+ * @See util.Timeout
+ */
 public abstract class Timeoutable implements Comparable<Timeoutable> {
     private Instant timeout;
     private boolean deleted = false;
