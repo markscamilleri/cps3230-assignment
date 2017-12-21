a63dd4935e93d8d9c37c4a141540ff0fec84166a
Miguel206 <dinglimiguel97@gmail.com>
2017-12-21 18:15:57 +0100

    Added GetNCharacters for use in tests.

--- src/main/java/util/Utils.java
+++ src/main/java/util/Utils.java
@@ -14,4 +14,19 @@ public class Utils {
         }
         return randString.toString();
     }
+
+    public static String getNCharacters(int characterCount, String startWith) {
+
+        final StringBuilder nonRandString = new StringBuilder();
+        nonRandString.append(startWith);
+
+        for (int i = 0; nonRandString.length() < characterCount; i++) {
+            nonRandString.append((char) i);
+        }
+        return nonRandString.substring(0, characterCount);
+    }
+
+    public static String getNCharacters(int characterCount) {
+        return getNCharacters(characterCount, "");
+    }
 }
