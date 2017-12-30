dfef81d7274dab071eeb527dfb39286b0be50b06
Miguel206 <dinglimiguel97@gmail.com>
2017-12-30 09:57:16 +0100

    Removed getValue and value field which were not being used.

--- src/main/java/system/StatusCodes.java
+++ src/main/java/system/StatusCodes.java
@@ -1,25 +1,13 @@
 package system;
 
 public enum StatusCodes {
-    OK("OK"),
-    AGENT_DOES_NOT_EXIST("AGENT DOES NOT EXIST"),
-    AGENT_NOT_LOGGED_IN("AGENT NOT LOGGED IN"),
-    SESSION_KEY_INVALID_LENGTH("SESSION KEY INVALID LENGTH"),
-    SESSION_KEY_UNRECOGNIZED("SESSION KEY UNRECOGNIZED"),
-    MESSAGE_LENGTH_EXCEEDED("MESSAGE LENGTH EXCEEDED"),
-    MESSAGE_CONTAINS_BLOCKED_WORD("MESSAGE CONTAINS BLOCKED WORD"),
-    FAILED_TO_ADD_TO_MAILBOX("FAILED TO ADD TO MAILBOX"),
-    GENERIC_ERROR("GENERIC ERROR"); // intended for use in testing
-
-    private final String value;
-
-    StatusCodes(String value) {
-        this.value = value;
-    }
-
-    public String getValue() {
-        return value;
-    }
+    OK,
+    AGENT_DOES_NOT_EXIST,
+    AGENT_NOT_LOGGED_IN,
+    SESSION_KEY_INVALID_LENGTH,
+    SESSION_KEY_UNRECOGNIZED,
+    MESSAGE_LENGTH_EXCEEDED,
+    MESSAGE_CONTAINS_BLOCKED_WORD,
+    FAILED_TO_ADD_TO_MAILBOX,
+    GENERIC_ERROR // intended for use in testing
 }
-
-
