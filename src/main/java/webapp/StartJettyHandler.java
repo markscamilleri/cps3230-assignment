66a6b31902e68d234025deed1fcf88f95a882580
Miguel206 <dinglimiguel97@gmail.com>
2017-12-30 10:29:49 +0100

    Changed runnable to lambda according to IntelliJ's suggestion.

--- src/main/java/webapp/StartJettyHandler.java
+++ src/main/java/webapp/StartJettyHandler.java
@@ -9,35 +9,28 @@ import system.MessagingSystem;
 public class StartJettyHandler {
 
     private static final int PORT_NUMBER = 8080;
-    
-    public static void startServer() {
+
+    public static void main(String[] args) {
 
         final MessagingSystem messagingSystem = new MessagingSystem();
-        final Runnable runnable = new Runnable() {
-            @Override
-            public void run() {
-                final Server server = new Server(PORT_NUMBER);
-                try {
-                    server.getConnectors()[0].getConnectionFactory(HttpConnectionFactory.class);
+        final Runnable runnable = () -> {
+            final Server server = new Server(PORT_NUMBER);
+            try {
+                server.getConnectors()[0].getConnectionFactory(HttpConnectionFactory.class);
 
-                    ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-                    context.setContextPath("/");
-                    server.setHandler(context);
+                ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+                context.setContextPath("/");
+                server.setHandler(context);
 
-                    context.addServlet(new ServletHolder(new LoginServlet(messagingSystem)),"/login/*");
-                    context.addServlet(new ServletHolder(new SendMailServlet(messagingSystem)), "/sendmail/*");
-                    
-                    server.start();
-                    server.join();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+                context.addServlet(new ServletHolder(new LoginServlet(messagingSystem)),"/login/*");
+                context.addServlet(new ServletHolder(new SendMailServlet(messagingSystem)), "/sendmail/*");
+
+                server.start();
+                server.join();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         };
         new Thread(runnable).start();
-    }
-
-    public static void main(String[] args) {
-        startServer();
     }
 }
