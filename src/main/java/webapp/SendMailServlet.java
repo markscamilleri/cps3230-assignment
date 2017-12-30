c207ae56e515b8458517f3066254564a43438f00
Miguel206 <dinglimiguel97@gmail.com>
2017-12-30 20:32:37 +0100

    Cleanup.

--- src/main/java/webapp/SendMailServlet.java
+++ src/main/java/webapp/SendMailServlet.java
@@ -35,11 +35,11 @@ public class SendMailServlet extends HttpServlet {
         } else {
             final String id = idCookie.getValue();
             final String sessionKey = skCookie.getValue();
-            
+
             String sendingMessageStatusText = "";
             Cookie statusCookie = Utils.findCookie(request.getCookies(), CookieNames.MESSAGE_SENDING_STATUS.name());
-            if (statusCookie != null){
-                if(statusCookie.getValue().equals(StatusCodes.OK.name()))
+            if (statusCookie != null) {
+                if (statusCookie.getValue().equals(StatusCodes.OK.name()))
                     sendingMessageStatusText = "Message Sent Successfully";
                 else if (statusCookie.getValue().equals(StatusCodes.AGENT_DOES_NOT_EXIST.name()))
                     sendingMessageStatusText = "That agent does not exist";
@@ -51,23 +51,20 @@ public class SendMailServlet extends HttpServlet {
                     sendingMessageStatusText = "Failed to add the message to the destination mailbox";
                 else if (statusCookie.getValue().equals(StatusCodes.GENERIC_ERROR.name()))
                     sendingMessageStatusText = "An error occured when sending your message";
-                // these should not happen, since the idCookie or skCookie should be deleted.
+                    // these should not happen, since the idCookie or skCookie should be deleted.
                 else if (statusCookie.getValue().equals(StatusCodes.SESSION_KEY_UNRECOGNIZED) ||
-                                 statusCookie.getValue().equals(StatusCodes.AGENT_NOT_LOGGED_IN.name()) ||
-                                 statusCookie.getValue().equals(StatusCodes.SESSION_KEY_INVALID_LENGTH.name()))
+                        statusCookie.getValue().equals(StatusCodes.AGENT_NOT_LOGGED_IN.name()) ||
+                        statusCookie.getValue().equals(StatusCodes.SESSION_KEY_INVALID_LENGTH.name()))
                     response.sendRedirect("/login");
-                
-                    
-    
             }
-            
+
             final boolean hasMessages = messagingSystem.agentHasMessages(sessionKey, id);
             final String mailboxMessage = hasMessages ? "You have new messages" : "You have no new messages";
 
             response.getWriter().println("" +
                     "<h1>Mailbox</h1>\n" +
-                    "<h2>Hello Agent "+ id  + "</h2>\n" +
-                    "<p class=\"notification\">"+ sendingMessageStatusText +"</p>" +
+                    "<h2>Hello Agent " + id + "</h2>\n" +
+                    "<p class=\"notification\">" + sendingMessageStatusText + "</p>" +
                     "    <div id=\"mailboxBlock\" class=\"inbox\">\n" +
                     "    <p id=\"mailboxMessagae\">" + mailboxMessage + "</p>\n" +
                     "    <button id=\"consumeMessage\" " + (hasMessages ? "" : "disabled") + "> Get Next Message </button>\n" +
@@ -83,6 +80,7 @@ public class SendMailServlet extends HttpServlet {
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
         final Cookie idCookie = Utils.findCookie(request.getCookies(), CookieNames.ID.name());
         final Cookie skCookie = Utils.findCookie(request.getCookies(), CookieNames.SKEY.name());
 
@@ -96,8 +94,8 @@ public class SendMailServlet extends HttpServlet {
             final String message = request.getParameter("messageBody");
 
             StatusCodes status = messagingSystem.sendMessage(sessionKey, id, destination, message);
-            
-            switch(status) {
+
+            switch (status) {
                 case SESSION_KEY_UNRECOGNIZED:
                 case AGENT_NOT_LOGGED_IN:
                 case SESSION_KEY_INVALID_LENGTH:
@@ -106,7 +104,7 @@ public class SendMailServlet extends HttpServlet {
                     skCookie.setMaxAge(0);
                     skCookie.setValue(null);
                     break;
-    
+
                 default:
                     response.addCookie(new Cookie(CookieNames.MESSAGE_SENDING_STATUS.name(), status.name()));
             }
