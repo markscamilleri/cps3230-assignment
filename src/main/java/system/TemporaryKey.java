fd7ce1e3f4b0d8a906a78d19c0de4c27233eef96
Mark Said Camilleri <markscamilleri@gmail.com>
2018-01-02 15:21:21 +0100

    Made it to actually expire after Duration

--- src/main/java/system/TemporaryKey.java
+++ src/main/java/system/TemporaryKey.java
@@ -8,7 +8,8 @@ import java.time.Instant;
 
 class TemporaryKey extends Timeoutable {
 
-    public final String key;
+    // Null when timed out
+    private String key;
 
     TemporaryKey(String key, Duration timeLimit) {
         this(key, timeLimit, Clock.systemUTC());
@@ -16,10 +17,19 @@ class TemporaryKey extends Timeoutable {
 
     TemporaryKey(String key, Duration timeLimit, Clock clock) {
         super(Instant.now(clock).plus(timeLimit));
-        this.key = key;
+    
+        this.key = isExpired()? null : key;
     }
-
+    
+    public String getKey() {
+        key = isExpired()? null : key;
+    
+        return key;
+    }
+    
     public boolean equals(String anotherKey) {
+        key = isExpired()? null : key;
+        
         return this.key.equals(anotherKey);
     }
 }
