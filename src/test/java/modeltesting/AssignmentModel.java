d0dddfc9c9ac47a448ba353aed5e853d8bf5d762
Mark Said Camilleri <markscamilleri@gmail.com>
2018-01-10 18:02:30 +0100

    Added getState and reset()

--- src/test/java/modeltesting/AssignmentModel.java
+++ src/test/java/modeltesting/AssignmentModel.java
@@ -1,11 +1,31 @@
 package modeltesting;
 
 import nz.ac.waikato.modeljunit.Action;
+import nz.ac.waikato.modeljunit.FsmModel;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-public class AssignmentModel {
+public class AssignmentModel implements FsmModel {
+    WebDriver driver = new ChromeDriver();
+    
+    ModelStateEnum currentState =  ModelStateEnum.UNREGISTERED;
     
     @Action
     private void login(){
     
+    }
+    
+    @Override
+    public Object getState() {
+        return currentState;
+    }
+    
+    @Override
+    public void reset(boolean b) {
+        currentState = ModelStateEnum.UNREGISTERED;
+    
+        if (b) {
+            driver = new ChromeDriver();
+        }
     }
 }
