86f7e0350e353795593aa69e2dc3ae4c20847719
Miguel206 <dinglimiguel97@gmail.com>
2018-01-10 23:17:43 +0100

    Added normalRegister, spyRegister, validLogin, invalidLogin.

--- src/test/java/modeltesting/AssignmentModel.java
+++ src/test/java/modeltesting/AssignmentModel.java
@@ -19,65 +19,64 @@ public class AssignmentModel implements FsmModel {
     private WebDriver driver = new ChromeDriver();
     private ModelStateEnum currentState = ModelStateEnum.UNREGISTERED;
     
-    private String agentID;
+    private String agentID = null;
+    private String loginKey = null;
     
     @Action
     public void register() {
-        driver.get(baseUrl + "/register");
-        
-        agentID = new Random().nextBoolean() ? "" : "spy-";
-        agentID += Utils.getNRandomCharacters(5);
-        
-        driver.findElement(By.id("idInput")).click();
-        driver.findElement(By.id("idInput")).sendKeys(agentID);
-        driver.findElement(By.id("submit")).click();
-        
         currentState = ModelStateEnum.SUPERVISOR_CHECKING;
     }
-    
-    public boolean registerGuard() {
+
+    @Action
+    public void normalRegister() {
+        agentID = Utils.getNRandomCharacters(5);
+        registerAgent(driver, agentID);
+
+        Assert.assertTrue(driver.getCurrentUrl().endsWith(baseUrl + "/login"));
+        currentState = ModelStateEnum.REGISTERED;
+    }
+
+    public boolean normalRegisterGuard() {
+        return currentState == ModelStateEnum.UNREGISTERED;
+    }
+
+    @Action
+    public void spyRegister() {
+        agentID = "spy-" + Utils.getNRandomCharacters(5);
+        registerAgent(driver, agentID);
+
+        Assert.assertTrue(driver.getCurrentUrl().endsWith(baseUrl + "/register"));
+        currentState = ModelStateEnum.UNREGISTERED;
+    }
+
+    public boolean spyRegisterGuard() {
         return currentState == ModelStateEnum.UNREGISTERED;
     }
     
     @Action
-    public void supervisorCheck() {
-        if (agentID.startsWith("spy-")) {
-            Assert.assertTrue(driver.getCurrentUrl().endsWith(baseUrl + "/register"));
-            currentState = ModelStateEnum.UNREGISTERED;
-        } else {
-            Assert.assertTrue(driver.getCurrentUrl().endsWith(baseUrl + "/login"));
-            currentState = ModelStateEnum.REGISTERED;
-        }
-    }
-    
-    public boolean supervisorCheckGuard() {
-        return currentState == ModelStateEnum.SUPERVISOR_CHECKING;
-    }
-    
-    @Action
-    public void login() {
-        final String LOGIN_KEY = driver.findElement(By.id("lKey")).getText();
-        
-        driver.findElement(By.id("lKeyInput")).click();
-        driver.findElement(By.id("lKeyInput")).sendKeys(LOGIN_KEY);
-        driver.findElement(By.id("submit")).click();
-        
+    public void validLogin() {
+        loginKey = driver.findElement(By.id("lKey")).getText();
+        loginAgent(driver, loginKey);
+
+        Assert.assertTrue(driver.getCurrentUrl().endsWith(baseUrl + "/sendmail"));
         currentState = ModelStateEnum.LOGIN_KEY_CHECKING;
     }
     
-    public boolean loginGuard() {
+    public boolean validLoginGuard() {
         return currentState == ModelStateEnum.REGISTERED;
     }
-    
+
     @Action
-    public void checkLoginKey() {
-    
+    public void invalidLogin() {
+        loginAgent(driver, "invalidLoginKey");
+
+        Assert.assertTrue(driver.getCurrentUrl().endsWith(baseUrl + "/login"));
+        currentState = ModelStateEnum.LOGIN_KEY_CHECKING;
     }
-    
-    public boolean checkLoginKeyGuard() {
-        return currentState == ModelStateEnum.LOGIN_KEY_CHECKING;
+
+    public boolean invalidLoginGuard() {
+        return currentState == ModelStateEnum.REGISTERED;
     }
-    
     
     @Override
     public Object getState() {
@@ -87,9 +86,36 @@ public class AssignmentModel implements FsmModel {
     @Override
     public void reset(boolean b) {
         currentState = ModelStateEnum.UNREGISTERED;
-        
+        agentID = null;
+        loginKey = null;
+
         if (b) {
             driver = new ChromeDriver();
         }
+    }
+
+    /**
+     * Registers the agent. No checks are done if this was successful
+     *
+     * @param driver  the WebDriver to use
+     * @param agentId the agent id to login
+     */
+    private static void registerAgent(WebDriver driver, String agentId) {
+        driver.get(baseUrl + "/register");
+        driver.findElement(By.id("idInput")).click();
+        driver.findElement(By.id("idInput")).sendKeys(agentId);
+        driver.findElement(By.id("submit")).click();
+    }
+
+    /**
+     * Logs in the agent on the specified WebDriver. No checks are done.
+     *
+     * @param driver   the WebDriver to use
+     * @param loginKey the loginKey to use
+     */
+    private static void loginAgent(WebDriver driver, String loginKey) {
+        driver.findElement(By.id("lKeyInput")).click();
+        driver.findElement(By.id("lKeyInput")).sendKeys(loginKey);
+        driver.findElement(By.id("submit")).click();
     }
 }
