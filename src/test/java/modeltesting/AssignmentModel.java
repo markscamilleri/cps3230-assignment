36488fbe972fb3284853c6bbfbc0d7437dd18427
Mark Said Camilleri <markscamilleri@gmail.com>
2018-01-10 17:12:32 +0100

    Page Enum

---
+++ src/test/java/modeltesting/AssignmentModel.java
@@ -0,0 +1,13 @@
+package modeltesting;
+
+public class AssignmentModel {
+
+
+    public enum Page {
+        REGISTER,
+        LOGIN,
+        LOGOUT,
+        SEND_MAIL,
+        READ_MAIL
+    }
+}
