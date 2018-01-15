5e763bcaa64278ad894cc8340897691466a4b6e9
Miguel206 <dinglimiguel97@gmail.com>
2018-01-15 22:29:30 +0100

    Added space.

--- src/main/java/system/AgentInfo.java
+++ src/main/java/system/AgentInfo.java
@@ -3,10 +3,11 @@ package system;
 import java.time.Duration;
 
 class AgentInfo {
+
     final Mailbox mailbox;
     TemporaryKey loginKey = new TemporaryKey("", Duration.ZERO); // initially, expired key
     TemporaryKey sessionKey = new TemporaryKey("", Duration.ZERO); // initially, expired key
- 
+
     int messagesSent = 0;
     int messagesRecv = 0;
 
